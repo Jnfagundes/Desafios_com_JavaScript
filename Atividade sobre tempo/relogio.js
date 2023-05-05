5e93526d7dc651cb19b8d074e8b5b8fc25f9e471
José Nilton <71674292+Jnfagundes@users.noreply.github.com>
2023-05-05 10:54:07 -0300

    Um novo relógio

---
+++ Atividade sobre tempo/relogio.js
@@ -0,0 +1,23 @@
+
+function relogioDois(){/*Criando a função relogio()*/
+            var data=new Date(); /*Aqui está sendo criado um objeto */
+            var hor=data.getHours();
+            var min=data.getMinutes();
+            var seg=data.getSeconds();
+            
+            if(hor < 10){
+                hor="0"+hor;
+            }
+            if(min < 10){
+                min="0"+min;
+            }
+            if(seg < 10){
+                seg="0"+seg;
+            }
+            
+            var horas= hor + ":" + min + ":" + seg;/**/
+            
+            document.getElementById("relogio").value=horas;
+        }
+
+var timer=setInterval(relogioDois,1000);
